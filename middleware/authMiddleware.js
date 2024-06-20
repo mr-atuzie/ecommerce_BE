9e0539b86da518cd4cbf2dd1c6f8b6021a925e11
mr-atuzie <user email>
2024-06-20 14:21:15 +0100

    token middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,10 +6,10 @@ const protect = asyncHandler(async (req, res, next) => {
   try {
     const token = req.cookies.token;
 
-    if (!token) {
-      res.status(401);
-      throw new Error("Not authorized, please login");
-    }
+    // if (!token) {
+    //   res.status(401);
+    //   throw new Error("Not authorized, please login");
+    // }
 
     //Verify token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
