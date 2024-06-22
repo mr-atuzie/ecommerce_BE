ca55d1a274d7aeb13bf8886ecfedb03ba3d5abd8
mr-atuzie <user email>
2024-06-22 03:49:30 +0100

    authmiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,35 +3,35 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/user");
 
 const protect = asyncHandler(async (req, res, next) => {
-  try {
-    const token = req.cookies.token;
+  // try {
+  const token = req.cookies.token;
 
-    // if (!token) {
-    //   res.status(401);
-    //   throw new Error("Not authorized, please login");
-    // }
+  // if (!token) {
+  //   res.status(401);
+  //   throw new Error("Not authorized, please login");
+  // }
 
-    //Verify token
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
+  //Verify token
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (!verified) {
-      res.status(401);
-      throw new Error("Token has expired, please login");
-    }
-
-    const user = await User.findById(verified.id).select("-password");
-
-    if (!user) {
-      res.status(401);
-      throw new Error("User not found");
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
+  if (!verified) {
     res.status(401);
-    throw new Error(error);
+    throw new Error("Token has expired, please login");
   }
+
+  const user = await User.findById(verified.id).select("-password");
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  req.user = user;
+  next();
+  // } catch (error) {
+  //   res.status(401);
+  //   throw new Error(error);
+  // }
 });
 //rex
 
